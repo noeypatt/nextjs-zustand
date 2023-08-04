dc7709a6585c6a5cc35d8f89e923cc1ece997e17
noey.patnarin <noey.patnarin@bitkub.com>
2023-08-04 15:54:02 +0700

    :recycle: refactor code

--- src/store/modules/useBoundStore/index.ts
+++ src/store/modules/useBoundStore/index.ts
@@ -4,7 +4,7 @@ import { BearSlice } from '../Bear/bear.interface'
 import { createFishSlice } from '../Fish'
 import { FishSlice } from '../Fish/fish.interface'
 
-export const useBoundStore = create<BearSlice & FishSlice>()((...a) => ({
+export const useBoundStore = create<BearSlice & FishSlice>((...a) => ({
   ...createBearSlice(...a),
   ...createFishSlice(...a),
 }))
